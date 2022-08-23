99ab72640867a3700e54084662738b09b4011dfc
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-08-24 01:16:18 +0200

    back to original src folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'potfolio'`, () => {
+  it(`should have as title 'myportfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('potfolio');
+    expect(app.title).toEqual('myportfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('potfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myportfolio app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/partials/nav-bar/nav-bar.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ProjectsComponent } from './components/pages/projects/projects.component';
-import { FooterComponent } from './components/partials/footer/footer.component';
-import { ResumeComponent } from './components/pages/resume/resume.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    HomeComponent,
-    ProjectsComponent,
-    FooterComponent,
-    ResumeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PdfViewerModule
-
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
