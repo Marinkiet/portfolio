88cf1ccaf67c367d828733f7ca5f84b7d2f96f35
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-08-24 01:13:04 +0200

    new src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './components/partials/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { ResumeComponent } from './components/pages/resume/resume.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ProjectsComponent,
+    FooterComponent,
+    ResumeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PdfViewerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
